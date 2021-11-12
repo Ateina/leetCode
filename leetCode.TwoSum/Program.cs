b9235236ab20b51ee5083afbb158619d1b643353
Ateina <totora246@gmail.com>
2021-11-13 01:31:31 +0300

    Two Sum : https://leetcode.com/problems/two-sum/

---
+++ leetCode.TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace leetCode.TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = new int[];
+            int[] result = TwoSum(nums, 9);
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int compliment = target - nums[i];
+                if (dict.ContainsKey(compliment))
+                {
+                    return new int[] { i, dict.GetValueOrDefault(compliment) };
+                }
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+
+            }
+            return null;
+        }
+    }
+}
