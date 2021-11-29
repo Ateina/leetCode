aaff1e37b8a816f062642f21ea44854279dae3b9
Ateina <totora246@gmail.com>
2021-11-30 01:31:05 +0300

    https://leetcode.com/problems/longest-common-prefix/
    https://leetcode.com/problems/palindrome-number/

---
+++ leetCode.IsPolindrom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace leetCode.IsPolindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 122221;
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
---
+++ leetCode.LongestCommonPrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace leetCode.LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = {"flower","flow","flight"};
+            string output = "";
+            int minLength = strs.Min(y=>y.Length); // this gets you the shortest length of all elements in names
+            string word = strs[0];
+            for (int i = 0; i < minLength; i++)
+            {
+                char letter = word[i];
+                Boolean equal = true;
+                for(int j = 1; j < strs.Length; j++){
+                    
+                    if(letter != strs[j][i]) {equal = false; break;}
+                }
+                if(equal) output = output + letter;
+                else{break;}
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
