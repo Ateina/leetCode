e6327d3e66e7e1e256f7833dc8a33009b1ee55f2
Antanina Druzhkina <a.druzhkina@brimit.com>
2022-05-14 01:04:00 +0400

    Valid Parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = IsValid("401716832807512840963");
+            Console.WriteLine(result);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsValid(string s)
+        {
+            while (IsItSomethingToRemove(s))
+            {
+                s = CheckForBrackets("()", s);
+                s = CheckForBrackets("{}", s);
+                s = CheckForBrackets("[]", s);
+            }
+
+            return s.Length == 0;
+        }
+
+        private static string CheckForBrackets(string bracketsType, string stringToUpdate)
+        {
+            int bracketsIndex = stringToUpdate.IndexOf(bracketsType);
+            if (bracketsIndex != -1)
+            {
+                stringToUpdate = stringToUpdate.Remove(bracketsIndex, 2);
+            }
+            return stringToUpdate;
+        }
+
+        private static bool IsItSomethingToRemove(string str)
+        {
+            if (str.IndexOf("()") != -1) return true;
+            if (str.IndexOf("{}") != -1) return true;
+            if (str.IndexOf("[]") != -1) return true;
+            return false;
+        }
+    }
+}
