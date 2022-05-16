ce283b6ebb647a66df9ceb6f175d44bec2326cb5
Antanina Druzhkina <a.druzhkina@brimit.com>
2022-05-16 20:31:16 +0400

    FirstBadVersion https://leetcode.com/problems/first-bad-version/submissions/

---
+++ FirstBadVersion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FirstBadVersion(int n)
+        {
+            int low = 1;
+            int high = n;
+            int mid = low + (high - low) / 2;
+            bool firstBadVersionWasFound = false;
+            while (!firstBadVersionWasFound)
+            {
+                if (IsBadVersion(mid) && !IsBadVersion(mid - 1))
+                {
+                    firstBadVersionWasFound = true;
+                    return mid;
+                }
+                else
+                {
+                    if (IsBadVersion(mid))
+                    {
+                        high = mid - 1;
+                        mid = low + (high - low) / 2;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                        mid = low + (high - low) / 2;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private bool IsBadVersion(int number)
+        {
+            return true;
+        }
+    }
+}
